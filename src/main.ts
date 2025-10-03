c2392ceaa5672d2c3d6c729df110677ef4b3a883
Muzamil Ali <muzamil_ali92@yahoo.com>
2025-10-03 18:10:41 +0500

    'hdd'

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
 
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
