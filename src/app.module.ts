704e6b821405fc44cbe02bc41074e7e4b5796eb2
Muzamil Ali <muzamil_ali92@yahoo.com>
2025-10-03 18:16:48 +0500

    'jdjd'

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: '127.0.0.1',
       port: 3306,
       username: 'root',
       password: '',
